c4ab20bdf82f9fde11b8cbd7551c50ebdca23f11
kabik <k2.s1k.x20@gmail.com>
2019-12-30 14:03:23 +0900

    Create template

---
+++ abc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+no=$1
+prefix="abc"
+problems=("a" "b" "c")
+template_file="template.cc"
+main_file="main.cc"
+input_file="input.txt"
+
+if [ -z "$no" ]; then
+    echo "usage: ./abc.sh problem_no"
+    exit 0
+fi
+
+for i in ${problems[@]}; do
+    prob_dir=${prefix}${no}/${i}
+    mkdir -p ${prob_dir}
+    cp -i $template_file ${prob_dir}/${main_file}
+    touch ${prob_dir}/${input_file}
+done
---
+++ template.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    string s;
+    cin >> a >> b >> s;
+
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << s << "\n";
+
+    return 0;
+}
