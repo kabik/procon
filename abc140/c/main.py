b151708c6416ff49681bc2c43a04fdb76671eaf8
kabik <k2.s1k.x20@gmail.com>
2020-01-21 22:25:07 +0900

    ABC140 C

--- abc140/c/main.py
+++ abc140/c/main.py
@@ -1,4 +1,8 @@
-s = input()
 n = int(input())
-a, b = map(int, input().split())
-l = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+sum = b[0] + b[-1]
+for i in range(1, n-1):
+    sum += min(b[i-1], b[i])
+
+print(sum)
