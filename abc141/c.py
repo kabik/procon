bd83c87e26d2037a0d178d30e612886f5cfd0f8f
kabik <k2.s1k.x20@gmail.com>
2020-01-23 09:11:22 +0900

    ABC141 C

--- abc141/c.py
+++ abc141/c.py
@@ -1,4 +1,18 @@
-s = input()
-n = int(input())
-a, b = map(int, input().split())
-l = list(map(int, input().split()))
+n, k, q = map(int, input().split())
+
+a = [-1] * q
+for i in range(q):
+    a[i] = int(input())
+
+t = [0] * (n+1)
+for i in range(q):
+    t[ a[i] ] += 1
+
+p = [k - q + t[i] for i in range(0, n+1)]
+p[0] = 0
+
+for i in range(1, n+1):
+    if p[i] > 0:
+        print('Yes')
+    else:
+        print('No')
