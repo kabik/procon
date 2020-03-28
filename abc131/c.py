1b2b261380f11791ec0685ab351d1e940024e75a
kabik <k2.s1k.x20@gmail.com>
2020-03-29 02:03:20 +0900

    ABC131 C

--- abc131/c.py
+++ abc131/c.py
@@ -1,1 +1,15 @@
-a,b,c,d = map(int, input().split())
+A,B,C,D = map(int, input().split())
+
+def gcd(a, b):
+    while b > 0:
+        a,b = b, a%b
+    return a
+
+def count(n):
+    c = n//C
+    d = n//D
+    g = gcd(C, D)
+    l = C*D//g
+    return n -c -d +n//l
+
+print(count(B) - count(A-1))
