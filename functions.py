977c8e9d4db80ed39236e370ed8dcd0397599abf
kabik <k2.s1k.x20@gmail.com>
2020-03-28 18:10:06 +0900

    Add GCD

--- functions.py
+++ functions.py
@@ -12,3 +12,16 @@ def conb(a, b):
         deno *= i + 1
 
     return int(nume / deno)
+
+"""
+return gcd
+"""
+def gcd_step(a, b):
+    return b, a % b
+
+def gcd(a, b):
+    a, b = gcd_step(a,b)
+    while b > 0:
+        a,b = gcd_step(a,b)
+
+    return a
