4a5a226c6cb84cda589663130294461d904b3c53
kabik <k2.s1k.x20@gmail.com>
2020-04-27 01:17:28 +0900

    ABC158 E

--- abc158/e.py
+++ abc158/e.py
@@ -1,12 +1,22 @@
-# TLE
+N,P = map(int, input().split())
+S = input()
 
-n, p = map(int, input().split())
-s = input()
-
-cnt = 0
-for i in range(0, n):
-    for j in range(i, n):
-        if int(s[i:j+1]) % p == 0:
-            cnt += 1
-
-print(cnt)
+if P == 2 or P == 5:
+    ans = 0
+    for i in range(N):
+        if int(S[i]) % P == 0:
+            ans += i+1
+    print(ans)
+else:
+    mod_cnt = [0]*P
+    mod_cnt[0] = 1
+    k = 0.1
+    t = 0
+    for i in range(1, N+1):
+        k = int(k * 10) % P
+        t = (t + k * int(S[-i])) % P
+        mod_cnt[t] += 1
+    ans = 0
+    for cnt in mod_cnt:
+        ans += (cnt * (cnt-1)) // 2
+    print(ans)
