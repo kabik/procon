4dab854a287f8b29867cb16e90e8ee042e6fbf5f
kabik <k2.s1k.x20@gmail.com>
2020-05-23 12:25:58 +0900

    ABC123 C

--- abc123/c.py
+++ abc123/c.py
@@ -1,6 +1,8 @@
-s = input()
-n = int(input())
-a, b = map(int, input().split())
-l = list(map(int, input().split()))
-l = [int(input()) for _ in range(n)]
-l = [list(map(int, input().split())) for _ in range(n)]
+N = int(input())
+L = []
+for _ in range(5):
+    L.append(int(input()))
+
+mn = min(L)
+ans = (-(-N//mn)) + 4
+print(ans)
