d79d00ad2125a6c24f63992627aa63984c1ef458
kabik <k2.s1k.x20@gmail.com>
2021-02-24 00:18:14 +0900

    ABC129 C

--- abc129/c.py
+++ abc129/c.py
@@ -1,22 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
-A = [int(input()) for _ in range(M)]
-broken = set(A)
-mod = 1000000007
+broken = [False]*N
+for _ in range(M):
+    a = int(input())
+    broken[a] = True
 
-unreachable = False
-for i in range(M-1):
-    if A[i+1] - A[i] == 1:
-        unreachable = True
-        break
-
-pat = [0]*(N+2)
-pat[0] = 1
-if unreachable:
-    print(0)
-else:
-    for i in range(N):
-        if i in broken:
-            continue
-        pat[i+1] = (pat[i+1] + pat[i]) % mod
-        pat[i+2] = (pat[i+2] + pat[i]) % mod
-    print(pat[N])
+MOD = 10**9+7
+P = [0]*(N+3)
+P[0] = 1
+for i in range(N):
+    if not broken[i]:
+        P[i+1] = (P[i+1] + P[i]) % MOD
+        P[i+2] = (P[i+2] + P[i]) % MOD
+print(P[N])
