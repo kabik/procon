d862f9c73fed861aa61addaf062a9be3c1a4d28a
kabik <k2.s1k.x20@gmail.com>
2021-06-08 22:10:36 +0900

    ABC097 C

---
+++ atcoder/abc097/c.py
@@ -0,0 +1,28 @@
+import sys
+
+# sys.setrecursionlimit(10**6)
+int1 = lambda x: int(x) - 1
+p2D = lambda x: print(*x, sep="\n")
+def II(): return int(sys.stdin.buffer.readline())
+def LI(): return list(map(int, sys.stdin.buffer.readline().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def LI1(): return list(map(int1, sys.stdin.buffer.readline().split()))
+def LLI1(rows_number): return [LI1() for _ in range(rows_number)]
+def BI(): return sys.stdin.buffer.readline().rstrip()
+def SI(): return sys.stdin.buffer.readline().rstrip().decode()
+INF = 10 ** 16
+MOD = 10 ** 9 + 7
+# MOD = 998244353
+
+import collections
+
+S = SI()
+K = II()
+N = len(S)
+
+l = set()
+for i in range(N):
+    for j in range(i, min(N, i+K)):
+        l.add(S[i:j+1])
+l = sorted(set(l))
+print(l[K-1])
