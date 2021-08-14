c0404bf4cabc64d1404f9586f3e6dd1383d75d96
kabik <k2.s1k.x20@gmail.com>
2021-08-14 23:33:54 +0900

    add size function

--- algorithms/union-find.py
+++ algorithms/union-find.py
@@ -1,21 +1,22 @@
 par = [i for i in range(N)]
 rank = [0]*N
-
+_size = [1]*N
 def root(x):
     if par[x] != x:
         par[x] = root(par[x])
     return par[x]
-
 def same(x, y):
     return root(x) == root(y)
-
+def size(x):
+    return _size[root(x)]
 def unite(x, y):
     x, y = root(x), root(y)
     if x == y: return
-
     if rank[x] < rank[y]:
+        _size[y] += _size[x]
         par[x] = y
     else:
+        _size[x] += _size[y]
         par[y] = x
         if rank[x] == rank[y]:
             rank[x] += 1
