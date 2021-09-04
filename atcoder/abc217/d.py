c09e34a0cf01a508e33ad5f33c4f3da74b112ae0
kabik <k2.s1k.x20@gmail.com>
2021-09-04 23:52:44 +0900

    ABC217 ABCDE

---
+++ atcoder/abc217/d.py
@@ -0,0 +1,32 @@
+import sys
+
+# sys.setrecursionlimit(10**6)
+input = sys.stdin.buffer.readline
+int1 = lambda x: int(x) - 1
+p2D = lambda x: print(*x, sep="\n")
+def II(): return int(input())
+def LI(): return list(map(int, input().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def LI1(): return list(map(int1, input().split()))
+def LLI1(rows_number): return [LI1() for _ in range(rows_number)]
+def BI(): return input().rstrip()
+def SI(): return input().rstrip().decode()
+def LSI(): return SI().split()
+def LLSI(rows_number): return [SI() for _ in range(rows_number)]
+INF = 10 ** 16
+MOD = 10 ** 9 + 7
+# MOD = 998244353
+
+import bisect
+from array import array
+
+L,Q = LI()
+CX = LLI(Q)
+
+P = array("i", [0,L])
+for c,x in CX:
+    if c == 1:
+        bisect.insort(P, x)
+    else:
+        k = bisect.bisect_left(P, x)
+        print(P[k]-P[k-1])
