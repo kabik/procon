8c32d37f82a0962ab9fd525419c987d1a47fceaa
kabik <k2.s1k.x20@gmail.com>
2021-09-22 15:16:45 +0900

    virtual contest

---
+++ atcoder/abc133/c2.py
@@ -0,0 +1,29 @@
+import sys
+
+# sys.setrecursionlimit(10**6)
+input = sys.stdin.buffer.readline
+int1 = lambda x: int(x) - 1
+p2D = lambda x: print(*x, sep="\n")
+def II(): return int(input())
+def LI(): return list(map(int, input().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def LI1(): return list(map(int1, input().split()))
+def LLI1(rows_number): return [LI1() for _ in range(rows_number)]
+def BI(): return input().rstrip()
+def SI(): return input().rstrip().decode()
+def LSI(): return SI().split()
+def LLSI(rows_number): return [SI() for _ in range(rows_number)]
+INF = 10 ** 16
+MOD = 2019
+# MOD = 998244353
+
+L,R = LI()
+
+if R - L >= MOD:
+    print(0)
+else:
+    ans = INF
+    for i in range(L, R):
+        for j in range(i+1, R+1):
+            ans = min(ans, (i*j) % MOD)
+    print(ans)
