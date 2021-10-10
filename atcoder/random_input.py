a8af2375d879b71a1ae169c55450656fc393cc74
kabik <k2.s1k.x20@gmail.com>
2021-10-10 11:00:57 +0900

    random string

--- atcoder/random_input.py
+++ atcoder/random_input.py
@@ -1,10 +1,37 @@
-import random
+import random, string
 
-h = int(random.randint(2, 5))
-w = int(random.randint(2, 5))
-c = int(random.randint(1, 10**9))
-A = [[int(random.randint(1, 10)) for _ in range(w)] for _ in range(h)]
+def random_string(options: str, length: int) -> str:
+    return ''.join(random.choices(options, k=length))
 
-print(h, w, c)
+def random_string_lower(length: int) -> str:
+    return random_string(options=string.ascii_lowercase, length=length)
+
+def random_string_upper(length: int) -> str:
+    return random_string(options=string.ascii_uppercase, length=length)
+
+def random_string_letters(length: int) -> str:
+    return random_string(options=string.ascii_letters, length=length)
+
+def random_string_digits(length: int) -> str:
+    return random_string(options=string.digits, length=length)
+
+def random_string_mix(length: int) -> str:
+    return random_string(options=string.ascii_letters + string.digits, length=length)
+
+
+H = int(random.randint(2, 5)) # 2 <= h <= 5
+W = int(random.randint(2, 5))
+A = [[int(random.randint(1, 10)) for _ in range(W)] for _ in range(H)]
+N = int(random.randint(1, 10))
+
+print(H, W)
 for a in A:
     print(*a)
+
+print(N)
+
+print(random_string('BW', N))
+print(random_string_upper(N))
+print(random_string_letters(N))
+print(random_string_digits(N))
+print(random_string_mix(N))
