0384e3fd46d60eff6d0714050a380c6750de9b73
kabik <k2.s1k.x20@gmail.com>
2022-01-20 23:22:32 +0900

    abc234 c

---
+++ atcoder/abc234/src/c.rs
@@ -0,0 +1,20 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        k: usize,
+    }
+
+    let mut ok = false;
+    let mut b: usize = 1 << 60;
+    for _ in (0..61).rev() {
+        if k & b > 0 {
+            print!("{}", 2);
+            ok = true;
+        } else if ok {
+            print!("{}", 0);
+        }
+        b >>= 1;
+    }
+    println!("");
+}
