19894b811c1221d55de56c91561ab13c0e07de5f
kabik <k2.s1k.x20@gmail.com>
2022-05-12 11:19:15 +0900

    typical90 ar

---
+++ atcoder/typical90/ar.py
@@ -0,0 +1,37 @@
+import sys
+
+# sys.setrecursionlimit(10**6)
+def debug(**kwargs): print(f'DEBUG: {kwargs}', file=sys.stderr)
+input = sys.stdin.buffer.readline
+int1 = lambda x: int(x) - 1
+p2D = lambda x: print(*x, sep="\n")
+def II(): return int(input())
+def LI(): return list(map(int, input().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def LI1(): return list(map(int1, input().split()))
+def LLI1(rows_number): return [LI1() for _ in range(rows_number)]
+def BI(): return input().rstrip()
+def SI(): return input().rstrip().decode()
+def LSI(): return SI().split()
+def LLSI(rows_number): return [SI() for _ in range(rows_number)]
+def YN(b): print({True: 'Yes', False: 'No'}[b])
+INF = 10 ** 16
+MOD = 10 ** 9 + 7
+# MOD = 998244353
+
+N, Q = LI()
+A = LI()
+
+shift = 0
+for _ in range(Q):
+    t,x,y = LI()
+    x2 = (x-1 - shift) % N
+    y2 = (y-1 - shift) % N
+    if t == 1:
+        tmp = A[y2]
+        A[y2] = A[x2]
+        A[x2] = tmp
+    elif t == 2:
+        shift += 1
+    else:
+        print(A[x2])
