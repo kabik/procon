8131fc3b4813b00337a4e5c84b243d208fb59e17
kabik <k2.s1k.x20@gmail.com>
2022-08-04 10:50:01 +0900

    typical90 bx

---
+++ atcoder/typical90/bx.py
@@ -0,0 +1,40 @@
+import sys
+
+# sys.setrecursionlimit(10**6)
+def debug(**kwargs): print(f'DEBUG: {kwargs}', file=sys.stderr)
+input = sys.stdin.buffer.readline
+int1 = lambda x: int(x) - 1
+p2D = lambda x: print(*x, sep="\n")
+def II(): return int(input())
+def LI(): return list(map(int, input().split()))
+def LLI(rows_number): return [LI() for _ in range(rows_number)]
+def LI1(): return list(map(int1, input().split()))
+def LLI1(rows_number): return [LI1() for _ in range(rows_number)]
+def BI(): return input().rstrip()
+def SI(): return input().rstrip().decode()
+def LSI(): return SI().split()
+def LLSI(rows_number): return [SI() for _ in range(rows_number)]
+def YN(b): print({True: 'Yes', False: 'No'}[b])
+INF = 10 ** 16
+MOD = 10 ** 9 + 7
+# MOD = 998244353
+
+N = II()
+A = LI()
+
+target = sum(A)/10
+
+sm = 0
+l = 0
+for r in range(2*N):
+    sm += A[r-N]
+    if sm == target:
+        print('Yes')
+        break
+    elif sm > target:
+        sm -= A[l-N]
+        l += 1
+    else:
+        pass
+else:
+    print('No')
